3f4332d6d85a0487c84b0ff530c40a1f8f9ce749
Cyril Levis <cyril@levis.name>
2021-07-28 00:09:56 +0200

    test(auth): update, use testify

--- auth/user.go
+++ auth/user.go
@@ -1,7 +1,7 @@
 package auth
 
 import (
-	"fmt"
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,20 +15,20 @@ type User struct {
 // NewUser create a new user
 func NewUser(username string, password string, role string) (*User, error) {
 	if username == "" {
-		return nil, fmt.Errorf("username can't be empty")
+		return nil, errors.New("username can't be empty")
 	}
 
 	if password == "" {
-		return nil, fmt.Errorf("password can't be empty")
+		return nil, errors.New("password can't be empty")
 	}
 
 	if role == "" {
-		return nil, fmt.Errorf("role can't be empty")
+		return nil, errors.New("role can't be empty")
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, fmt.Errorf("cannot hash password: %w", err)
+		return nil, errors.Wrap(err, "cannot hash password")
 	}
 
 	user := &User{
--- auth/user_test.go
+++ auth/user_test.go
@@ -1,76 +1,47 @@
 package auth
 
 import (
-	// "regexp"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-// TestNewUser create a new user with a name, password and user role
-// checking for a valid return value
-func TestNewUser(t *testing.T) {
-	user, err := NewUser("user1", "secret1", "user")
-	if err != nil {
-		t.Fatalf(`Something wrong %v %v`, err, user)
-	}
+func Test_NewUser_InvalidInput(t *testing.T) {
+	_, err := NewUser("", "secret1", "user")
+	assert.Error(t, err, "Should have failed with invalid username")
+
+	_, err = NewUser("admin2", "", "admin")
+	assert.Error(t, err, "Should have failed with invalid password")
+
+	_, err = NewUser("user3", "secret3", "")
+	assert.Error(t, err, "Should have failed with invalid role")
 }
 
-// TestNewUser create a new user with a name, password and user role
-// checking for a valid return value
-func TestNewUserWithEmptyUsername(t *testing.T) {
-	user, err := NewUser("", "secret1", "user")
-	if err == nil {
-		t.Fatalf(`Empty username should not be allow %v %v`, err, user)
-	}
+func Test_IsCorrectPassword_ValidatePassword(t *testing.T) {
+	goodpass := "goodpass"
+	user, _ := NewUser("user1", goodpass, "user")
+	assert.True(t, user.IsCorrectPassword("goodpass"), "Password should be ok")
+	assert.False(t, user.IsCorrectPassword("badpass"), "Password should not be ok")
+	assert.NotEqual(t, goodpass, user.HashedPassword, "Password should not be in cleartext")
 }
 
-// TestNewUserWithoutPassword create a new user with a name, without password and admin role
-// checking for a valid return value
-func TestNewUserWithoutPassword(t *testing.T) {
-	user, err := NewUser("admin2", "", "admin")
-	if err == nil {
-		t.Fatalf(`User with empty password should not be allowed: %#v`, user)
-	}
-}
+// func TestCloneUser(t *testing.T) {
+// 	user1, err := NewUser("user1", "secret1", "user")
+// 	if err != nil {
+// 		t.Fatalf(`Empty username should not be allow %v %v`, err, user1)
+// 	}
 
-// TestNewUserWithEmptyRole create a new user with a name, password and an empty role
-// checking for a valid return value
-func TestNewUserWithEmptyRole(t *testing.T) {
-	user, err := NewUser("user3", "secret3", "")
-	if err == nil {
-		t.Fatalf(`Empty role should not be allow %v %v`, err, user)
-	}
-}
+// 	user2, err := NewUser("user2", "secret2", "user")
+// 	if err != nil {
+// 		t.Fatalf(`Empty username should not be allow %v %v`, err, user2)
+// 	}
 
-// TestIsCorrectPassword username/password checking
-// checking for a valid return value
-func TestUsernamePassword(t *testing.T) {
-	user, err := NewUser("user1", "secret1", "user")
-	if err != nil {
-		t.Fatalf(`Something wrong %v %v`, err, user)
-	}
-	if ok := user.IsCorrectPassword("secret1"); !ok {
-		t.Fatalf(`Password checking failed %v`, err)
-	}
-}
+// 	user3 := user1.Clone()
+// 	if !(*user1 == *user3) {
+// 		t.Fatalf(`User1 and user3 must be equal %v == %v`, user1, user3)
+// 	}
 
-func TestCloneUser(t *testing.T) {
-	user1, err := NewUser("user1", "secret1", "user")
-	if err != nil {
-		t.Fatalf(`Empty username should not be allow %v %v`, err, user1)
-	}
+// 	if !(*user1 != *user2) {
+// 		t.Fatalf(`User1 and user2 must not be equal %v != %v`, user1, user2)
+// 	}
 
-	user2, err := NewUser("user2", "secret2", "user")
-	if err != nil {
-		t.Fatalf(`Empty username should not be allow %v %v`, err, user2)
-	}
-
-	user3 := user1.Clone()
-	if !(*user1 == *user3) {
-		t.Fatalf(`User1 and user3 must be equal %v == %v`, user1, user3)
-	}
-
-	if !(*user1 != *user2) {
-		t.Fatalf(`User1 and user2 must not be equal %v != %v`, user1, user2)
-	}
-
-}
+// }
