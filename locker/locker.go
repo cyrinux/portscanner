35991cc356edf0cc18d733c34580403b3aca8c4c
Cyril Levis <cyril@levis.name>
2021-07-29 17:34:52 +0200

    chore: add locker documentation

--- locker/locker.go
+++ locker/locker.go
@@ -3,10 +3,11 @@ package locker
 import (
 	"context"
 	"github.com/bsm/redislock"
-	redis "github.com/go-redis/redis/v8"
+	"github.com/go-redis/redis/v8"
 	"time"
 )
 
+// MyLocker create a mock locker interface
 type MyLocker interface {
 	Obtain(ctx context.Context, key string, ttl time.Duration) (bool, error)
 	Release(ctx context.Context, key string) error
@@ -19,11 +20,13 @@ type redisLocker struct {
 	locks  map[string]*redislock.Lock
 }
 
+// CreateRedisLock create a redis lock
 func CreateRedisLock(redisClient *redis.Client) MyLocker {
 	locker := redislock.New(redisClient)
 	return &redisLocker{locker: locker, locks: map[string]*redislock.Lock{}}
 }
 
+// Obtain get a lock from a key and duration
 func (l *redisLocker) Obtain(ctx context.Context, key string, ttl time.Duration) (bool, error) {
 	lock, err := l.locker.Obtain(ctx, key, ttl, nil)
 
@@ -40,14 +43,17 @@ func (l *redisLocker) Obtain(ctx context.Context, key string, ttl time.Duration)
 	return ok, err
 }
 
+// Release release the lock
 func (l *redisLocker) Release(ctx context.Context, key string) error {
 	return l.locks[key].Release(ctx)
 }
 
+// TTL return the life duration of the lock from the lock key
 func (l *redisLocker) TTL(ctx context.Context, key string) (time.Duration, error) {
 	return l.locks[key].TTL(ctx)
 }
 
+// Refresh add some time to the ttl life from the lock key and time to add
 func (l *redisLocker) Refresh(ctx context.Context, key string, ttl time.Duration) error {
 	return l.locks[key].Refresh(ctx, ttl, nil)
 }
